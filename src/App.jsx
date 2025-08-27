72a304003f41a8556b21989b4b36c96a53b3e91c
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-08-28 02:07:00 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 import Card from "./components/Card";
 
 function Home() {
-  return <Card title="Home" content="Welcome to the home page" />;
+  return <Card title="Home" content="Welcome to the home page ok" />;
 }
 function About() {
   return <Card title="About" content="This is about the app." />;
